baeae1ca81b79572e83d7404edcfee4a256b79bf
John Vidler <j.vidler@lancaster.ac.uk>
2011-04-27 18:16:08 +0100

    + Missed off the main method! How on Earth did I manage that!

---
+++ src/uk/co/johnvidler/BibTeXMgr/BibTeXMgr.java
@@ -0,0 +1,28 @@
+package uk.co.johnvidler.BibTeXMgr;
+
+import uk.co.johnvidler.BibTeXMgr.panels.ListTaskPanel;
+import uk.co.johnvidler.BibTeXMgr.ui.MainWindow;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: john
+ * Date: 30/03/11
+ * Time: 16:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class BibTeXMgr
+{
+
+    public static void main( String args[] ){ new BibTeXMgr( args ); }
+    public BibTeXMgr( String args[] )
+    {
+
+        MainWindow win = new MainWindow();
+
+        ListTaskPanel listTask = new ListTaskPanel();
+
+        win.setCurrentTaskPanel( listTask );
+
+    }
+
+}
